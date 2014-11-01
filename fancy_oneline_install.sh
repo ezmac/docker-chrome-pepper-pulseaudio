2c2195763ebc85f65182b1fc7f6e8aa243e36ce9
Tad <tadmerchant@library.msstate.edu>
2014-11-01 17:38:19 -0500

    finish? fancy oneline installer
    Don't know that it works any/everywhere

--- fancy_oneline_install.sh
+++ fancy_oneline_install.sh
@@ -2,9 +2,10 @@ curl -sSL https://get.docker.com/ | sh
 docker pull ezmac/docker-chrome-pulseaudio
 echo "load-module module-native-protocol-tcp">>~/.config/pulse/default.pa
 sudo service pulseaudio restart
+docker pull ezmac/google-chrome
 
 docker run -ti --rm \
        -e DISPLAY=$DISPLAY \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /home/$USER/.pulse-cookie:/home/chrome/.pulse-cookie:rw\
-       google-chrome --no-sandbox
+       ezmac/google-chrome --no-sandbox
