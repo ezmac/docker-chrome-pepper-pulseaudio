5d00ece30956873c64cb015802689290a14d45b4
Tad Merchant <system.root@gmail.com>
2014-11-03 09:10:30 -0600

    Fancy oneline install doesn't work correctly
    use it as an example only.

--- fancy_oneline_install.sh
+++ fancy_oneline_install.sh
@@ -1,6 +1,6 @@
 curl -sSL https://get.docker.com/ | sh
-docker pull ezmac/docker-chrome-pulseaudio
-echo "load-module module-native-protocol-tcp">>~/.config/pulse/default.pa
+# Sorry, my pulseaudio install doesn't want to play nicely with me, so I can't really test this.
+sudo su 'echo "load-module module-native-protocol-tcp">>~/.config/pulse/default.pa'
 sudo service pulseaudio restart
 docker pull ezmac/google-chrome
 
